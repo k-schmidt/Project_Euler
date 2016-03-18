548867db7adce9a05130e238af06df3e12d3fb64
Kyle Schmidt <kas2207@columbia.edu>
2016-03-18 17:35:15 -0400

    finished Racket #5, smallest multiple

--- Racket/005.rkt
+++ Racket/005.rkt
@@ -1,23 +1,22 @@
 #lang racket
 
+#|What is the smallest positive number that is evenly
+divisible by all of the numbers from 1 to 20?
+|#
+
 (define (non-factors x)
   (for/list ([i (in-range 2 x)]
     #:unless (equal? (remainder x i) 0))
     i))
 
-(define (is not-divisible-by i m)
-  (cond
-    [(= i 1) #true]
-    [else (cond
-            [(= (remainder m i) 0) #false]
-            [else (is-not-divisible-by (sub1 i) m)])]))
-
-(define (recur-divisible i x)
-  (for ([y (in-list i)]
-    #:break (not (zero? (remainder x y))))
-    (display x)))
+(define (is-not-divisible-by i m)
+  (if (null? i) m
+      (if (zero? (remainder m (first i)))
+          (is-not-divisible-by (rest i) m)
+          #false)))
 
 (define (small-multi x)
-  (for*/first ([i (in-list (non-factors x))]
-               #:when (is-not-divisible-by i (* x i)))
-    (* i x)))
+  (let ([j (non-factors x)])
+    (for/first ([k (in-naturals 1)]
+                 #:when (is-not-divisible-by j (* k x)))
+      (* k x))))
