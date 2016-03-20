e4b1468efc0482789caa9854e0a7a24683aef7ee
Kyle Schmidt <kas2207@columbia.edu>
2016-03-19 20:53:02 -0400

    adding racket 007, 10,001st prime number

--- Racket/007.rkt
+++ Racket/007.rkt
@@ -1,13 +1,24 @@
 #lang racket
 
-(define (prime? p m)
-  (if (null? p) m
-      (if (zero? (remainder m (first p)))
-          #false
-          (prime? (rest p) m))))
+#| What is the 10,001st prime number?
+Not a very fast solution
+|#
 
-(define (prime-number-index x)
-  (let ([p '(2)])
-    (for ([i (in-naturals 1)])
-      (if (prime? p i)
-          (cons i p)
+(define INIT-PRIMES '(2))
+
+(define (prime? primes a)
+  (cond
+    [(null? primes) #true]
+    [(not (zero? (remainder a (first primes)))) (prime? (rest primes) a)]
+    [else #false]))
+
+(define (get-prime nth primes a)
+  (cond
+    [(equal? nth (length primes)) (first primes)]
+    [(prime? primes a) (get-prime nth (cons a primes) (add1 a))]
+    [else (get-prime nth primes (add1 a))]))
+
+(define (nth-prime n)
+  (get-prime n INIT-PRIMES 3))
+
+(nth-prime 10001)
