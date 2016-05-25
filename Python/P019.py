a37b67ff5f7cffbaaac79e2805147066181eacd7
kyleschmidt <kyleschmidt@debian.Godel>
2016-05-24 23:34:58 -0400

    commiting python problem 19

--- Python/P019.py
+++ Python/P019.py
@@ -9,8 +9,6 @@ def div_4(year):
 
 class Year:
 
-    days_of_week = {1, 2, 3, 4, 5, 6, 7}
-    
     def __init__(self, year):
         "Used to calculate data about the days in the year"
         self.year = year
@@ -49,16 +47,24 @@ def get_start_day_of_year_given_previous(year, start_day=2):
     _, rem = divmod(yr.year_days, len(yr.days_of_week))
     return start_day + rem
 
+def increase_weekday(day):
+    if day == 7:
+        return 1
+    return day + 1
+
 def iter_year(start_year, end_year):
     start_day = get_start_day_of_year_given_previous(start_year - 1)
+    count = 0
     for year in range(start_year, end_year + 1):
         yr = Year(year)
-        i = 0
         for month in yr.months_days:
-            i += month
-            print(i)
-
+            for day in range(1, month + 1):
+                if day == 1 and start_day == 1:
+                    count += 1
+                start_day = increase_weekday(start_day)
+                print(year, month, day, start_day, count)
+    return count
 
 if __name__ == '__main__':
     print(get_start_day_of_year_given_previous(1900))
-    iter_year(1901, 2000)
+    print(iter_year(1901, 2000))
